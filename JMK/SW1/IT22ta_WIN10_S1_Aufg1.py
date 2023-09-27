89e08424f78a5b77439c0176357fcccc766ef2be
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2023-09-27 11:27:39 +0200

    aufgabe 2 in progress
    Co-authored-by: Jan Kott <boostvolt@users.noreply.github.com>

--- JMK/SW1/IT22ta_WIN10_S1_Aufg1.py
+++ JMK/SW1/IT22ta_WIN10_S1_Aufg1.py
@@ -2,18 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(-10, 10, 0.1)
-f = (x ** 5) - (5 * (x ** 4)) - (30 * (x ** 3)) + (110 * (x ** 2)) + (29 * x) -105
-f_deriv = (5 * x ** 4) - (20 * x ** 3) - 90 * x ** 2 + 220 * x + 29
-f_integ = (1/6) * x ** 6 - x ** 5 - (30/4) * x ** 4 + (110/3) * x ** 3 + (29/2) * x ** 2 - 105 * x
+f = (x**5) - (5 * (x**4)) - (30 * (x**3)) + (110 * (x**2)) + (29 * x) - 105
+f_deriv = (5 * x**4) - (20 * x**3) - 90 * x**2 + 220 * x + 29
+f_integ = (
+    (1 / 6) * x**6
+    - x**5
+    - (30 / 4) * x**4
+    + (110 / 3) * x**3
+    + (29 / 2) * x**2
+    - 105 * x
+)
 
 plt.plot(x, f)
 plt.plot(x, f_deriv)
 plt.plot(x, f_integ)
-plt.xlabel('x axis')
-plt.ylabel('y axis')
+plt.xlabel("x axis")
+plt.ylabel("y axis")
 plt.xlim(-7.5, 7.5, 0.5)
 plt.ylim(-250, 250, 1)
-plt.title('Polynomial')
-plt.legend(['f(x)', 'f_deriv', 'f_integral'])
+plt.title("Polynomial")
+plt.legend(["f(x)", "f_deriv", "f_integral"])
 plt.grid()
 plt.show()
--- JMK/SW1/IT22ta_WIN10_S1_Aufg2.py
+++ JMK/SW1/IT22ta_WIN10_S1_Aufg2.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+vector = np.array([3, 2, 5, 3, 1])
+deriv_vector = np.array([])
+i = len(vector)
+position = 0
+while i > 0:
+    exponent = i - 1
+    vector[position] = vector[position] * exponent
+
+    i = i - 1
+    position = position + 1
+
+print(deriv_vector)
