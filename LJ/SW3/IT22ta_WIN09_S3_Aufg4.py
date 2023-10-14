a2865178f9c7b4684ddf3e62782ebe0515d1ed54
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-10-14 13:49:45 +0200

    Final

--- LJ/SW3/IT22ta_WIN09_S3_Aufg4.py
+++ LJ/SW3/IT22ta_WIN09_S3_Aufg4.py
@@ -3,16 +3,12 @@ import matplotlib.pyplot as plt
 
 # Aufgabe 4 b)
 
-x = np.arange(1.1, 1.3, 10 ** -8)
+x = np.arange(1.1, 1.3, 10 ** -7)
 h = (100 * (x -1) * x) / (100 * x ** 2 - 200 * x +99)
 plt.semilogy(x, h)
-plt.ylim(0, 10 ** 15)
-
 plt.xlabel('x axis')
 plt.ylabel('y axis')
 plt.title('Konditionszahl')
 plt.legend(['f(x)'])
 plt.grid()
 plt.show()
-
-
