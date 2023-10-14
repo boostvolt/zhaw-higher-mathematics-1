699d95cdbe7fceaa7a63274dfc41adc9e7d083c0
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-10-14 13:49:32 +0200

    finale Aufgabe 4

--- LJ/SW3/IT22ta_WIN09_S3_Aufg4.py
+++ LJ/SW3/IT22ta_WIN09_S3_Aufg4.py
@@ -1,20 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(1.00000001, 1.1, 10 ** (-7))
-h = ((100 * (x ** 2)) - (200 * x) + 99) ** (1/2)
-plt.loglog(x, h)
+# Aufgabe 4 b)
 
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-plt.title('Koordinatensystem mit logarhitmischer x- und y-Achse')
-plt.legend(['f(x)'])
-plt.grid()
-plt.show()
-
-x = np.arange(1.1, 1.3, 10 ** -7)
+x = np.arange(1.1, 1.3, 10 ** -8)
 h = (100 * (x -1) * x) / (100 * x ** 2 - 200 * x +99)
 plt.semilogy(x, h)
+plt.ylim(0, 10 ** 15)
 
 plt.xlabel('x axis')
 plt.ylabel('y axis')
