eb3522b45bfbe2d06bac547076e4f63bc2692f59
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-10-14 13:31:56 +0200

    feat: add task 4 [JMK]

--- JMK/SW3/IT22ta_WIN10_S3_Aufg3b.py
+++ JMK/SW3/IT22ta_WIN10_S3_Aufg3b.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.logspace(1, 101)
+x = np.arange(1, 101)
+
 plt.loglog(x, 5 / ((2 * x**2) ** (1 / 3)), label="f(x)")
 plt.legend()
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
-x = np.arange(1, 101)
 plt.semilogy(x, 10**5 * ((2 * np.e) ** (-x / 100)), label="g(x)")
 plt.legend()
 plt.xlabel("x")
