1429946a53503fcc2ab34c37c0138606a6265ba4
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-10-19 23:27:31 +0200

    feat: add praktikum 4

---
+++ JMK/SW4/IT22ta_WIN10_S4_Aufg2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def fix_point_iteration(alpha, k):
+    return alpha * k * (1 - k)
+
+k0 = 0.1
+alpha_values = np.arange(0, 8.1, 0.5)
+for alpha in alpha_values:
+    k = k0
+    print(f"alpha = {alpha}")
+    for i in range(21):
+        k = fix_point_iteration(alpha, k)
+        print(f"k = {k}")
+
+# Aufgabe 2 
+# a) Ab 21 Iteration haben alpha 0, 1.5, 2, 2.5 einen Fixpunkt.
+# b) Der Fixpunkt gibt an bei welcher Infektionsrate die Anzahl erkrankten und gesunden Kindern stagniert. (Gleich viele gesund wie krank)
