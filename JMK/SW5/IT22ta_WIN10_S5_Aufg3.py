b8d0cdbc2e24e6e95b2c9590bca5725d4db39f17
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-10-24 20:49:50 +0200

    feat: add equations from task 1 and 2 for task 3 function testing

--- JMK/SW5/IT22ta_WIN10_S5_Aufg3.py
+++ JMK/SW5/IT22ta_WIN10_S5_Aufg3.py
@@ -9,5 +9,6 @@ def secant(f, x0, x1, tol):
 
 
 print(secant(lambda x: np.e ** (x**2) + x ** (-3) - 10, -1.0, -1.2, 10 ** (-3)))
+print(secant(lambda x: -(1/3) * np.pi * x ** 3 + 5 * np.pi * x ** 2 -471, 9, 7.6582, 10 ** (-3)))
 
 # Beim Netwon-Verfahren muss man f noch ableiten, was je nach f sehr mühsam sein könnte.
