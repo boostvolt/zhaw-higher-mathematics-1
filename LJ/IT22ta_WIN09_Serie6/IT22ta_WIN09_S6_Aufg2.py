9520978ccd8c408de40c834bb54c8acaa29e4bd6
leiselau <leiselau@students.zhaw.ch>
2023-10-31 13:03:49 +0100

    Serie6

--- LJ/IT22ta_WIN09_Serie6/IT22ta_WIN09_S6_Aufg2.py
+++ LJ/IT22ta_WIN09_Serie6/IT22ta_WIN09_S6_Aufg2.py
@@ -1,1 +1,66 @@
 import numpy as np
+
+def obereDreiecksMatrix(A,b):
+    # überprüfen dass Matrix quadratisch ist:
+    if (len(A) != len(A[0])):
+        return
+    
+    n = len(A)
+    numberOfRowSwaps = 0
+
+    for i in range(len(A[0])): 
+
+        # Nullen auf Diagonale eliminieren
+
+        if ((A[i,i] == 0)):
+            rowToSwapWith = -1
+            for j in range(i + 1, n):
+                if ((A[j,i] != 0) and (rowToSwapWith == -1)):
+                    rowToSwapWith = j
+            if(rowToSwapWith == -1):
+                print("invalid")
+                return
+            
+            copyA = np.copy(A)
+            A[i] = copyA[rowToSwapWith]
+            A[rowToSwapWith] = copyA[i]
+
+            copyb = np.copy(b)
+            b[i] = copyb[rowToSwapWith]
+            b[rowToSwapWith] = copyb[i]
+
+            numberOfRowSwaps += 1
+
+        # Eliminationsschritt
+
+        for j in range(i + 1, n):
+            b[j] = b[j] - A[j,i]/A[i,i] * b[i] 
+            A[j] = A[j] - A[j,i]/A[i,i] * A[i] 
+            # evtl b korrigieren
+
+    A_triangle = A
+
+    # Determinante berechnen
+
+    detA = -1 ** numberOfRowSwaps
+    for i in range(n):
+        detA *= A[i,i]
+    
+    # auflösen nach x
+
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            b[i] -= A[i,j] * x[j]
+
+        x[i] = b[i] / A[i,i]
+
+    return [A_triangle, detA, x]
+
+
+# Wird nur ausgeführt wenn explizit dieses Script ausgeführt wird. 
+if __name__ == "__main__":
+
+    A = np.array([[1, 5, 6], [7,9,6], [2,3,4]], dtype=float)
+    b = np.array([29,43,20], dtype=float)
+    print(obereDreiecksMatrix(A,b))
--- LJ/IT22ta_WIN09_Serie6/IT22ta_WIN09_S6_Aufg3.py
+++ LJ/IT22ta_WIN09_Serie6/IT22ta_WIN09_S6_Aufg3.py
@@ -1,1 +1,16 @@
 import numpy as np
+from IT22ta_WIN09_S6_Aufg2 import obereDreiecksMatrix
+
+A1 = np.array([[-1,1,1],[1,-3,-2],[5,1,4]], dtype=float)
+c1 = np.array([13,-32,22], dtype=float)
+
+A2 = np.copy(A1)
+c2 = np.copy(c1)
+
+[triangle, det, x1] = obereDreiecksMatrix(A1,c1)
+
+x2 = np.linalg.solve(A2,c2)
+
+print("Das Resultat mit unserer Berechnung lautet {}. Das Resultat mit numpy.linalg.solve() lautet: {}".format(x1,x2))
+
+# Für die oben angegebene Matrix  und c lautet x = [-1,7,5] für beide Rechenwege. Es gibt keine Unterschiede.
