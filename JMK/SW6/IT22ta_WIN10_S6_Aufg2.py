3b6b85c0e52efc79fd037a3ce979c53e751bdfc7
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2023-11-05 18:40:42 +0100

    use old implentation as final version

--- JMK/SW6/IT22ta_WIN10_S6_Aufg2.py
+++ JMK/SW6/IT22ta_WIN10_S6_Aufg2.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 def gaussian_algorithm(matrix, result_vector):
-    matrix = np.copy(matrix.astype(np.float64))
-    result_vector = np.copy(result_vector.astype(np.float64))
+    print("mk")
+    matrix = matrix.astype(np.float64)
+    result_vector = result_vector.astype(np.float64)
 
     if len(matrix) != len(matrix[0]):
         raise ValueError("Error: The matrix is not square")
@@ -12,51 +13,72 @@ def gaussian_algorithm(matrix, result_vector):
         raise ValueError("Error: The matrix and result vector have different sizes")
 
     n = len(matrix)
-    det = 1
+    matrix_top_triangle = np.copy(matrix).astype(np.float64)
+    result_vector_b = np.copy(result_vector).astype(np.float64)
+    det_a = 1
 
     for column in range(n):
-        # Find the row with the lowest index which is not zero in the current column
-        non_zero_rows = np.nonzero(matrix[column:, column])[0]
-        if non_zero_rows.size == 0:
-            raise ValueError("Error: A column is all zero")
-        first_non_zero_row = column + non_zero_rows[0]
+        if matrix_top_triangle[column, column] == 0:
+            zero_columns = 0
+            for row in range(column + 1, n):
+                if matrix_top_triangle[row, column] == 0:
+                    zero_columns += 1
+                else:
+                    for row_next_value in range(column + 1, n):
+                        current_row_matrix = np.copy(matrix_top_triangle[column, :])
+                        current_row_result = np.copy(result_vector_b[column])
 
-        # If the first non-zero row is not the current row, swap them
-        if column != first_non_zero_row:
-            matrix[[column, first_non_zero_row]] = matrix[[first_non_zero_row, column]]
-            result_vector[[column, first_non_zero_row]] = result_vector[
-                [first_non_zero_row, column]
-            ]
+                        if (
+                            row_next_value >= column + 1
+                            and matrix_top_triangle[row_next_value, column] != 0
+                        ):
+                            matrix_top_triangle[column, :] = matrix_top_triangle[
+                                row_next_value, :
+                            ]
+                            matrix_top_triangle[row_next_value, :] = current_row_matrix
+                            result_vector_b[column] = result_vector_b[row_next_value]
+                            result_vector_b[row_next_value] = current_row_result
 
-            det *= -1
+                            det_a *= -1
 
-        # Make all rows below this one 0 in the current column
+            if zero_columns == n - column - 1:
+                raise ValueError("Error: A column is all zero")
+
         for row in range(column + 1, n):
-            factor = matrix[row, column] / matrix[column, column]
-            matrix[row, column:] -= factor * matrix[column, column:]
-            result_vector[row] -= factor * result_vector[column]
+            factor = (
+                matrix_top_triangle[row, column] / matrix_top_triangle[column, column]
+            )
+            matrix_top_triangle[row, column:] -= (
+                factor * matrix_top_triangle[column, column:]
+            )
+            result_vector_b[row] -= factor * result_vector_b[column]
 
-    # Calculate the determinant
-    det_matrix = det * np.prod(np.diag(matrix))
+    det_matrix = calc_det(matrix_top_triangle, det_a)
 
-    # Check if the determinant is zero
     if det_matrix == 0:
         raise ValueError("Error: det is zero")
 
-    # Perform back substitution to solve for x
-    x = reverse_substitution(matrix, result_vector)
-
-    return matrix, det_matrix, x
+    return (
+        matrix_top_triangle,
+        det_matrix,
+        reverse_substitution(matrix_top_triangle, result_vector_b),
+    )
 
 
 def reverse_substitution(matrix, result_vector):
     n = len(matrix)
     x = np.zeros(n)
-
     for diagonal in range(n - 1, -1, -1):
-        x[diagonal] = (
-            result_vector[diagonal]
-            - np.dot(matrix[diagonal, diagonal + 1 :], x[diagonal + 1 :])
-        ) / matrix[diagonal, diagonal]
+        for column in range(diagonal + 1, n):
+            result_vector[diagonal] -= matrix[diagonal, column] * x[column]
+
+        x[diagonal] = result_vector[diagonal] / matrix[diagonal, diagonal]
 
     return x
+
+
+def calc_det(matrix, det_a):
+    for diagonal in range(len(matrix) - 1, -1, -1):
+        det_a *= matrix[diagonal, diagonal]
+
+    return det_a
