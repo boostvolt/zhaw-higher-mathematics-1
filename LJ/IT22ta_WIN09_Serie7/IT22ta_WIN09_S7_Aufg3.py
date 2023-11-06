4f35bdf5a41adb7082138d44aa7172348553ac1d
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-11-06 18:28:55 +0100

    s7_3

--- LJ/IT22ta_WIN09_Serie7/IT22ta_WIN09_S7_Aufg3.py
+++ LJ/IT22ta_WIN09_Serie7/IT22ta_WIN09_S7_Aufg3.py
@@ -1,7 +1,40 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
+from IT22ta_WIN09_S6_Aufg2 import obereDreiecksMatrix
 
-A = np.array([[0.8, 2.2, 3.6], [2.0, 3.0, 4.0], [1.2, 2.0, 5.8]])
+# Aufgabe 3 
+# a)
 
-print(sp.linalg.lu(A))
+A = np.array([[0, 0, 0, 1], [8, 4, 2, 1], [729, 81, 9, 1], [2197, 169, 13, 1]])
+b = np.array([150, 104, 172, 152])
+x1 = np.arange(0, 13, 0.1)
 
+[triangle, det, x] = obereDreiecksMatrix(A,b)
+plt.plot(x1 + 1997, np.polyval(x, x1))
+plt.xlabel('Jahre')
+plt.ylabel('Tage')
+plt.title('Aufgabe 3b')
+plt.grid()
+plt.show()
+
+# b)
+# Für 2003 wird ein Wert von 126 Tage geschätzt.
+# Für 2004 wird ein Wert von 143 Tage geschätzt.
+
+# c)
+# Für 2003 wird ein Wert von 126 Tage geschätzt.
+# Für 2004 wird ein Wert von 143 Tage geschätzt.
+
+x2 = np.arange(1997, 2011, 0.1)
+x3 = np.polyfit(np.array([1997, 1999, 2006, 2010]), np.array([150, 104, 172, 152]), 3)
+
+print(x1)
+plt.plot(x1 + 1997, np.polyval(x, x1))
+plt.plot(x2, np.polyval(x3, x2))
+plt.xlabel('Jahr')
+plt.ylabel('Tage')
+plt.title('Aufgabe 3c')
+plt.legend(['3b', '3c'])
+plt.grid()
+plt.show()
