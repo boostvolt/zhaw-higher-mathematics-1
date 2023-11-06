3032f9fd18f1273095c9e400d61d339a384945b4
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-11-06 18:31:36 +0100

    refactor: add dotted line sw7

--- JMK/SW7/IT22ta_WIN10_S7_Aufg3.py
+++ JMK/SW7/IT22ta_WIN10_S7_Aufg3.py
@@ -1,10 +1,7 @@
-# This is a Python File
-
 import numpy as np
 import matplotlib.pyplot as plt
 from IT22ta_WIN10_S6_Aufg2 import gaussian_algorithm
 
-# Aufgabe 3a)
 A = np.array(
     [
         [0, 0, 0, 1],
@@ -16,22 +13,18 @@ A = np.array(
 b = np.array([150, 104, 172, 152])
 
 x = np.arange(0, 13, 0.1)
+y = np.array([150, 104, 172, 152])
 plt.figure()
-plt.plot(x + 1997, np.polyval(gaussian_algorithm(A, b)[2], x))
+plt.plot(x + 1997, np.polyval(gaussian_algorithm(A, b)[2], x), "-")
+coeff = np.polyfit(np.array([0, 2, 9, 13]), y, 3)
+plt.plot(x + 1997, np.polyval(coeff, x), ":")
 plt.ylim(90, 190, 1)
 plt.grid()
 plt.show()
 
-# Aufgabe 3b) Jahr 2003: 126.6 und Jahr 2004: 143.1
+# Aufgabe 3b)
+# Jahr 2003: 126.6 und Jahr 2004: 143.1
 
 # Aufgabe 3c)
-x = np.arange(0, 13, 0.1)
-y = np.array([150, 104, 172, 152])
-coeff = np.polyfit(np.array([0, 2, 9, 13]), y, 3)
-plt.figure()
-plt.plot(x + 1997, np.polyval(coeff, x))
-plt.grid()
-plt.show()
-
 # Grafisch sieht es identisch aus und mit polyfit bekommen wir diese folgende Werte:
 # Jahr 2003: 126.69 und Jahr 2004: 143.03
