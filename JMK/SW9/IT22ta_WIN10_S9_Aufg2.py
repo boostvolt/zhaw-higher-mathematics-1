a89d9219e63759eade74c06b4873b4e6e5d85031
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2023-11-20 17:44:48 +0100

    feat: finish all tasks

--- JMK/SW9/IT22ta_WIN10_S9_Aufg2.py
+++ JMK/SW9/IT22ta_WIN10_S9_Aufg2.py
@@ -6,16 +6,28 @@ def serie_9(A, A_tilde, b, b_tilde):
     x = np.linalg.solve(A, b)
     x_tilde = np.linalg.solve(A_tilde, b_tilde)
 
-    dx_max = (
-        l.cond(A, np.inf)
-        / (1 - l.cond(A, np.inf) * l.norm((A - A_tilde), np.inf) / l.norm(A, np.inf))
-    ) * (
-        l.norm((A - A_tilde), np.inf) / l.norm(A, np.inf)
-        + l.norm((b - b_tilde), np.inf) / l.norm(b, np.inf)
-    )
+    dx_max = np.nan
+    if l.cond(A, np.inf) * (l.norm((A - A_tilde), np.inf) / l.norm(A, np.inf)) < 1:
+        dx_max = (
+            l.cond(A, np.inf)
+            / (
+                1
+                - l.cond(A, np.inf) * l.norm((A - A_tilde), np.inf) / l.norm(A, np.inf)
+            )
+        ) * (
+            l.norm((A - A_tilde), np.inf) / l.norm(A, np.inf)
+            + l.norm((b - b_tilde), np.inf) / l.norm(b, np.inf)
+        )
 
-    dx_obs = (l.norm(x, np.inf) - l.norm(x_tilde, np.inf)) / (l.norm(x, np.inf))
+    dx_obs = (l.norm(x - x_tilde, np.inf)) / (l.norm(x, np.inf))
 
     return (x, x_tilde, dx_max, dx_obs)
 
-print()
+
+A = np.array([[20, 30, 10], [10, 17, 6], [2, 3, 2]])
+A_tilde = np.array([[19.9, 29.9, 9.9], [9.9, 16.9, 5.9], [1.9, 2.9, 1.9]])
+
+b = np.array([5720, 3300, 836])
+b_tilde = np.array([5820, 3400, 936])
+
+print(serie_9(A, A_tilde, b, b_tilde))
---
+++ JMK/SW9/IT22ta_WIN10_S9_Aufg3.py
@@ -0,0 +1,38 @@
+import numpy as np
+import numpy.random as r
+import matplotlib.pyplot as plt
+from IT22ta_WIN10_S9_Aufg2 import serie_9
+
+
+def random_iteration():
+    A = r.rand(100, 100)
+    A_tilde = A + r.rand(100, 100) / 10**5
+
+    b = r.rand(100, 1)
+    b_tilde = b + r.rand(100, 1) / 10**5
+
+    _, _, dx_max, dx_obs = serie_9(A, A_tilde, b, b_tilde)
+
+    return dx_max, dx_obs
+
+
+dx_max_list = []
+dx_obs_list = []
+ratio_list = []
+
+for _ in range(1000):
+    dx_max, dx_obs = random_iteration()
+    dx_max_list.append(dx_max)
+    dx_obs_list.append(dx_obs)
+    ratio_list.append(dx_max / dx_obs)
+
+x = np.arange(1, 1001)
+plt.figure()
+plt.semilogy(x, dx_max_list, label="dx_max")
+plt.semilogy(x, dx_obs_list, label="dx_obs")
+plt.semilogy(x, ratio_list, label="ratio")
+plt.legend()
+plt.grid()
+plt.show()
+
+# Ja, dx_max ist eine realistische obere Schranke für dx_obs. Weil dx_obs nie größer als dx_max ist.
