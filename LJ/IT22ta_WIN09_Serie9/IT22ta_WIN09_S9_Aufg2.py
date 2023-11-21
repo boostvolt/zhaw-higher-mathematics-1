73e95d48e18d86ab1773372364822df700b01e74
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-11-21 13:37:28 +0100

    aufgabe 1-3

---
+++ LJ/IT22ta_WIN09_Serie9/IT22ta_WIN09_S9_Aufg2.py
@@ -0,0 +1,30 @@
+import numpy.linalg as lin
+import numpy as np
+
+
+def serie_9(A, A_tilde, b, b_tilde):
+    x = np.linalg.solve(A, b)
+    x_tilde = np.linalg.solve(A_tilde, b_tilde)
+    relA = lin.norm((A - A_tilde), np.inf) / lin.norm(A, np.inf)
+    condARel = lin.cond(A, np.inf) / (1 - lin.cond(A, np.inf) * relA)
+    bRel = lin.norm((b - b_tilde), np.inf) / lin.norm(b, np.inf)
+    xRel = (lin.norm(x - x_tilde, np.inf)) / (lin.norm(x, np.inf))
+
+    dx_max = np.nan
+
+    if lin.cond(A, np.inf) * (lin.norm((A - A_tilde), np.inf) / lin.norm(A, np.inf)) < 1:
+
+        dx_max = condARel * (relA + bRel)
+
+    dx_obs = xRel
+
+    return (x, x_tilde, dx_max, dx_obs)
+
+
+A = np.array([[20, 30, 10], [10, 17, 6], [2, 3, 2]])
+A_tilde = np.array([[19.9, 29.9, 9.9], [9.9, 16.9, 5.9], [1.9, 2.9, 1.9]])
+
+b = np.array([5720, 3300, 836])
+b_tilde = np.array([5820, 3400, 936])
+
+print(serie_9(A, A_tilde, b, b_tilde))
---
+++ LJ/IT22ta_WIN09_Serie9/IT22ta_WIN09_S9_Aufg3.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IT22ta_WIN09_S9_Aufg2 import serie_9
+
+rng = np.random.default_rng(42)
+
+
+def random_iteration():
+    A = rng.random((100, 100))
+    A_tilde = A + rng.random((100, 100)) / 10**5
+
+    b = rng.random((100, 1))
+    b_tilde = b + rng.random((100, 1)) / 10**5
+
+    _, _, dx_max, dx_obs = serie_9(A, A_tilde, b, b_tilde)
+
+    return dx_max, dx_obs
+
+
+dx_max_list = []
+dx_obs_list = []
+ratio_list = []
+
+for _ in range(1000):
+    dx_max, dx_obs = random_iteration()
+    dx_max_list.append(dx_max)
+    dx_obs_list.append(dx_obs)
+    ratio_list.append(dx_max / dx_obs)
+
+x = np.arange(1, 1001)
+plt.figure()
+plt.semilogy(x, dx_max_list, label="dx_max")
+plt.semilogy(x, dx_obs_list, label="dx_obs")
+plt.semilogy(x, ratio_list, label="ratio")
+plt.legend()
+plt.grid()
+plt.show()
+
+# Ja, dx_max ist eine realistische obere Schranke für dx_obs. Weil dx_obs nie grösser als dx_max ist.
