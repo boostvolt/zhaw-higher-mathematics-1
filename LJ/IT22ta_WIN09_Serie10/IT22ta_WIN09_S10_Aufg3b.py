1c78359ca4c9021e36c9db717ec4ab5310cb22f0
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-11-28 13:44:57 +0100

    Aufgabe 3

--- LJ/IT22ta_WIN09_Serie10/IT22ta_WIN09_S10_Aufg3b.py
+++ LJ/IT22ta_WIN09_Serie10/IT22ta_WIN09_S10_Aufg3b.py
@@ -7,22 +7,54 @@ from sympy import false, true
 from IT22ta_WIN09_S10_Aufg3a import IT22ta_WIN09_S10_Aufg3a
 from IT22ta_WIN09_S6_Aufg2 import obereDreiecksMatrix
 
-
+#Aufgabe 3b
 dim = 3000
 A = np.diag(np.diag(np.ones((dim,dim))*4000))+np.ones((dim,dim))
 print(A)
 dum1 = np.arange(1,int(dim/2+1),dtype=np.float64).reshape((int(dim/2),1)) 
 dum2 = np.arange(int(dim/2),0,-1,dtype=np.float64).reshape((int(dim/2),1)) 
 x = np.append(dum1,dum2,axis=0)
-
 b = A@x
 x0 = np.zeros((dim,1))
 tol = 1e-4
-opt = false
+opt = true
 
-#t3=timeit("Serie8_Aufg2(Test)", "from __main__ import Serie8_Aufg2, Test") 
-#t4=timeit("np.linalg.qr(Test)", "from __main__ import np, Test")
-#t1 = timeit.timeit("IT22ta_WIN09_S10_Aufg3a(A, b, x0, tol, opt)", "from __main__ import IT22ta_WIN09_S10_Aufg3a, A, b, x0, tol, opt")
-#t2 = timeit.timeit(lambda: np.linalg.solve(A, b), number=1)
-#t3 = timeit.timeit("np.linalg.solve(A, b)", "from __main__ import np, A, b")
-#print(t2)
+#Linalg.solve() Laufzeit
+# t1 = timeit.timeit(lambda: np.linalg.solve(A, b), number=1)
+# #Jacobi Laufzeit
+# t2 = timeit.timeit(lambda: IT22ta_WIN09_S10_Aufg3a(A, b, x0, tol, opt), number=1)
+# #Gauss-Seidel Laufzeit
+# opt = false
+# t3 = timeit.timeit(lambda: IT22ta_WIN09_S10_Aufg3a(A, b, x0, tol, opt), number=1)
+# #Gauss Laufzeit
+# t4 = timeit.timeit(lambda: obereDreiecksMatrix(A, b), number=1)
+
+# print("Linagl.solve() Laufzeit: {}".format(t1))
+# print("Jacobi-Laufzeit: {}".format(t2))
+# print("Gauss-Seidel Laufzeit: {}".format(t3))
+# print("Gauss-Zerlegung Laufzeit: {}".format(t4))
+# print("Gauss braucht {} mal länger als die Gauss-Seidel-Funktion".format(t4/t3))
+# Linagl.solve() Laufzeit: 0.21742208395153284
+# Jacobi-Laufzeit: 22.236404209048487
+# Gauss-Seidel Laufzeit: 5.150750666973181
+# Gauss-Zerlegung Laufzeit: 28.33560229104478
+# Gauss braucht 5.5012568309186065 mal länger als die Gauss-Seidel-Funktion
+# Jedoch bricht unsere Gauss-Funktion schon nach 28 Sekunden ab aufgrund eines Overflows.
+# In Realität würde unsere Funktion daher noch deutlich länger benötigen als die eben berechnete Zeit.
+
+#Aufgabe 3c
+[xJacobi, _, _] = IT22ta_WIN09_S10_Aufg3a(A, b, x0, tol, true)
+[xGaussSeidel, _, _] = IT22ta_WIN09_S10_Aufg3a(A, b, x0, tol, false)
+plt.figure()
+plt.plot(np.abs(xJacobi - x), label="Jacobi")
+plt.plot(np.abs(xGaussSeidel - x), label="Gauss-Seidel")
+plt.legend()
+plt.xlabel("Vektor-Element")
+plt.ylabel("Absoluter Fehler")
+plt.title("Absoluter Fehler pro Vektorelement")
+plt.grid()
+plt.show()
+
+#Die Jacobi Funktion hat konstant einen absoluten Fehler von 4.1 * 10^-5
+#Die Gauss-Seidel Funktion schwankt stark zwischen 7.8 * 10^-6 und 0.
+#Das heisst die Gauss-Seidel Funktion berechnet das Ergebnis genauer als die Jacobi-Funktion.
