16b4b8248007c812edf312df010a597e0e0e93fb
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2023-12-22 12:30:46 +0100

    draft

---
+++ Kapitel 1/Ableiten_Integrieren.py
@@ -0,0 +1,20 @@
+from sympy import symbols, diff, cos, sin, pi, log
+
+
+def ableitung(funktion):
+    ableitung = diff(funktion, x)
+    return ableitung
+
+
+x = symbols("x")  # Unbekannten hier definieren
+
+# Beispielaufrufe
+# funktion = sympy.exp(x)  # Für e^x
+# funktion = sympy.sin(x)
+# funktion = sympy.cos(x)
+# funktion = log(x)
+# funktion = log(x, 2) # Mit Basis 2
+funktion = (x**3 + 4) ** -2  # Für (x^3 + 4)^(-2)
+
+resultat = ableitung(funktion)
+print(f"Die Ableitung von {funktion} ist: {resultat}")
---
+++ Kapitel 1/Konditionszahl.py
@@ -0,0 +1,27 @@
+from sympy import symbols, diff, pi
+import sympy
+
+
+def konditionszahl(funktion, variable, punkt):
+    ableitung_funktion = diff(funktion, variable)
+
+    f_wert = funktion.subs(variable, punkt)
+    f_ableitung_wert = ableitung_funktion.subs(variable, punkt)
+
+    if f_ableitung_wert != 0:
+        konditionszahl_wert = (abs(f_ableitung_wert) * abs(punkt)) / abs(f_wert)
+        return konditionszahl_wert
+    else:
+        return "Konditionszahl kann nicht berechnet werden (Ableitung an diesem Punkt ist Null)."
+
+
+# Beispielaufruf
+
+variable = symbols("x")  # Unbekannte hier definieren (x, y, z etc.)
+# funktion = sympy.sin(variable)  # Die Funktion hier angeben, beachte mit sympy Sachen wie cos, sin, exp zu benutzen!
+funktion = sympy.exp(variable)
+punkt = 2  # Der Punkt (x), an dem du die Konditionszahl berechnen möchtest
+
+
+resultat = konditionszahl(funktion, variable, punkt)
+print(f"Die Konditionszahl der Funktion {funktion} an x = {punkt} ist: {resultat}")
