e8fd47c1a1a0e5d2dab807d83c03b3048831c83e
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2023-12-30 20:22:02 +0100

    Add first version of a-posteriori

---
+++ Scripts/Kapitel 4/a-priori.py
@@ -0,0 +1,9 @@
+import numpy as np
+import numpy.linalg as lin
+
+toleranz = 10**(-4)
+A = np.array([[8, 5, 2], [5, 9, 1], [4, 2, 7]])
+b = np.array([19, 5, 34])
+
+# Jacobi
+B = -lin.inv(D).dot(L+R)
