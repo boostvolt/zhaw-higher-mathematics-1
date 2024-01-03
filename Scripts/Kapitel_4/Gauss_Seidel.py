1190b6dc346efb41984fdd07ce70b8c7280e5ab9
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-01-03 21:12:35 +0100

    improv: Add additional prints in Gauss-Seidel [KAPITEL 4]

--- Scripts/Kapitel_4/Gauss_Seidel.py
+++ Scripts/Kapitel_4/Gauss_Seidel.py
@@ -1,58 +1,93 @@
+import A_Matrix_in_D_L_R_unterteilen
 import numpy as np
 import numpy.linalg as lin
 from sympy import false, true
-import A_Matrix_in_D_L_R_unterteilen
 
 
-def gauss_seidel(prev_x):
+def gauss_seidel(prev_x, debug=False):
+    if debug:
+        print(
+            f"-> \n{-lin.inv(D + L).dot(R)}\n * \n{prev_x.reshape(-1, 1)}\n + \n{(lin.inv(D + L).dot(b)).reshape(-1, 1)}\n"
+        )
     return -lin.inv(D + L).dot(R).dot(prev_x) + lin.inv(D + L).dot(b)
+
+
+def print_gauss_seidel_zwischenschritte():
+    print("---------------------------------------")
+    print("x^k+1 = -inv(D + L) * R * x^0 + inv(D + L) * b \n")
+    print(
+        f"= -inv(\n{D + L}\n) * \n{R}\n * x_k + inv(\n{D + L}\n) * \n{b.reshape(-1, 1)}\n"
+    )
+    print("Vereinfacht: ")
+    print(
+        f"= \n{-lin.inv(D + L)}\n * \n{R}\n * x_k + \n{lin.inv(D + L)}\n * \n{b.reshape(-1, 1)}\n"
+    )
+    print("Vereinfacht: ")
+    print(
+        f"= \n{-lin.inv(D + L).dot(R)}\n * x_k + \n{(lin.inv(D + L).dot(b)).reshape(-1, 1)}\n"
+    )
+
 
 def gauss_seidel_vergleiche_toleranz(prev_value, next_value):
     for i in range(len(prev_value)):
-        if (np.abs(prev_value[i] - next_value[i]) > toleranz):
+        if np.abs(prev_value[i] - next_value[i]) > toleranz:
             return false
     return true
-    
-def gauss_seidel_mit_toleranz(startvektor):
+
+
+def gauss_seidel_mit_toleranz(startvektor, debug=False):
     previous_value = startvektor
     next_x_value = np.full(len(previous_value), -100)
     iteration_count = 0
 
-    while (not gauss_seidel_vergleiche_toleranz(previous_value, next_x_value)):
+    if debug:
+        print_gauss_seidel_zwischenschritte()
+
+    while not gauss_seidel_vergleiche_toleranz(previous_value, next_x_value):
         # Nur für die erste Iteration nicht machen
         if iteration_count > 0:
             previous_value = next_x_value
-        next_x_value = gauss_seidel(previous_value)
-        print(f"x^{iteration_count} = {previous_value} -> {next_x_value}")
-        iteration_count = iteration_count + 1
+        if debug:
+            print(f"Gauss-Seidel Iteration: {iteration_count + 1}")
+            print(f"x^{iteration_count} = \n{previous_value.reshape(-1, 1)}")
+        next_x_value = gauss_seidel(previous_value, debug)
+        print(f"= \n{next_x_value.reshape(-1, 1)}")
+        print("--------------")
     print("Fixpunkt: ", next_x_value)
     # Gebe Fixpunkt zurück
     return next_x_value
-    
 
 
-def gauss_seidel_mit_anzahl_iterationen(startvektor, anzahl_iterationen):
+def gauss_seidel_mit_anzahl_iterationen(startvektor, anzahl_iterationen, debug=False):
     previous_value = startvektor
     next_x_value = -100
     x_values = [previous_value]
+
+    if debug:
+        print_gauss_seidel_zwischenschritte()
 
     for i in range(anzahl_iterationen):
         # Nur für die erste Iteration nicht machen
         if i > 0:
             previous_value = next_x_value
-        next_x_value = gauss_seidel(previous_value)
+        if debug:
+            print(f"Gauss-Seidel Iteration: {i + 1}")
+            print(f"x^{i} = \n{previous_value.reshape(-1, 1)}")
+        next_x_value = gauss_seidel(previous_value, debug)
         x_values.append(next_x_value)
-        print(f"x^{i} = {previous_value} -> {next_x_value}")
+        print(f"= \n{next_x_value.reshape(-1, 1)}")
+        print("--------------")
     return x_values
+
 
 # Variablen definieren
 toleranz = 10**-4
-startvektor = np.array([1, -1, 3])
-A = np.array([[8, 5, 2], [5, 9, 1], [4, 2, 7]])
-b = np.array([19, 5, 34])
-[L, R, D] = A_Matrix_in_D_L_R_unterteilen.unterteilen(A)
+startvektor = np.array([4, 3, -0.5])
+A = np.array([[2, -1, 0], [-1, 4, 1], [0, 1, 2]])
+b = np.array([4, 5, -2])
+[L, R, D] = A_Matrix_in_D_L_R_unterteilen.unterteilen(A, True)
 
 # Fixpunkt mit Toleranz berechnen
 # fixpunkt = gauss_seidel_mit_toleranz(startvektor)
 # Fixpunkt mit Anzahl Iterationsschritten berechnen
-gauss_seidel_mit_anzahl_iterationen(startvektor, 4)
+gauss_seidel_mit_anzahl_iterationen(startvektor, 1, True)
