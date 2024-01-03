dcfb136ce471a9662f7ef805d105b8f75657939d
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2024-01-03 12:06:53 +0100

    refactor: add more debug outputs

---
+++ Scripts/Kapitel_4/vektor_normen.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def vektor_norm(vektor, norm):
+    norm_symbol = norm
+    if norm == np.inf:
+        norm_symbol = "∞"
+
+    # TODO: Add debug output
+
+    return norm_symbol, np.linalg.norm(vektor, norm)
+
+########################################################################################
+
+# Vektor definieren
+vektor = np.array([[1], [-2]])
+
+# Norm definieren (1, 2, np.inf)
+norm = np.inf
+
+[norm_symbol, norm] = vektor_norm(vektor, norm)
+print(f"Vektor {norm_symbol}-Norm: {norm}")
