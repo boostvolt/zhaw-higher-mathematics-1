b1f76e32a99ecb7d9e7c71d211e56b6de768d54b
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-01-03 21:10:01 +0100

    improv: Add additional prints in Jacobi

--- Scripts/Kapitel_4/A_Matrix_in_D_L_R_unterteilen.py
+++ Scripts/Kapitel_4/A_Matrix_in_D_L_R_unterteilen.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def unterteilen(A):
+
+def unterteilen(A, debug=False):
     D = np.diag(np.diag(A))
     R = np.triu(A) - D
     L = np.tril(A) - D
+    if debug:
+        print(f"L: \n {L}")
+        print(f"D: \n {D}")
+        print(f"R: \n {R}")
     return L, R, D
--- Scripts/Kapitel_4/Jacobi.py
+++ Scripts/Kapitel_4/Jacobi.py
@@ -1,58 +1,96 @@
+import A_Matrix_in_D_L_R_unterteilen
 import numpy as np
 import numpy.linalg as lin
 from sympy import false, true
-import A_Matrix_in_D_L_R_unterteilen
 
 
-def jacobi(prev_x):
-    return -lin.inv(D).dot(L+R).dot(prev_x) + lin.inv(D).dot(b)
+def jacobi(prev_x, debug=False):
+    if debug:
+        print(
+            f"-> \n{-lin.inv(D).dot(L + R)}\n * \n{prev_x.reshape(-1, 1)}\n + \n{(lin.inv(D).dot(b)).reshape(-1, 1)}\n"
+        )
+    return -lin.inv(D).dot(L + R).dot(prev_x) + lin.inv(D).dot(b)
+
+
+def print_jacobi_zwischenschritte():
+    print("---------------------------------------")
+    print("x^k+1 = -inv(D) * (L + R) * x^0 + inv(D) * b \n")
+    print(
+        f"= -inv(\n{D}\n) * \n{L + R}\n * x_k + inv(\n{D}\n) * \n{b.reshape(-1, 1)}\n"
+    )
+    print("Vereinfacht: ")
+    print(
+        f"= \n{-lin.inv(D)}\n * \n{L + R}\n * x_k + \n{lin.inv(D)}\n * \n{b.reshape(-1, 1)}\n"
+    )
+    print("Vereinfacht: ")
+    print(
+        f"= \n{-lin.inv(D).dot(L + R)}\n * x_k + \n{(lin.inv(D).dot(b)).reshape(-1, 1)}\n"
+    )
+
 
 def vergleiche_toleranz(prev_value, next_value):
     for i in range(len(prev_value)):
-        if (np.abs(prev_value[i] - next_value[i]) > toleranz):
+        if np.abs(prev_value[i] - next_value[i]) > toleranz:
             return false
     return true
-    
-def jacobi_mit_toleranz(startvektor):
+
+
+def jacobi_mit_toleranz(startvektor, debug=False):
     previous_value = startvektor
     next_x_value = np.full(len(previous_value), -100)
     iteration_count = 0
 
-    while (not vergleiche_toleranz(previous_value, next_x_value)):
+    if debug:
+        print_jacobi_zwischenschritte()
+
+    while not vergleiche_toleranz(previous_value, next_x_value):
         # Nur für die erste Iteration nicht machen
         if iteration_count > 0:
             previous_value = next_x_value
-        next_x_value = jacobi(previous_value)
-        print(f"x^{iteration_count} = {previous_value} -> {next_x_value}")
+        if debug:
+            print(f"Jacobi Iteration: {iteration_count + 1}")
+            print(f"x^{iteration_count} = \n{previous_value.reshape(-1, 1)}")
+        next_x_value = jacobi(previous_value, debug)
+        print(f"= \n{next_x_value.reshape(-1, 1)}")
         iteration_count = iteration_count + 1
     # Gebe Fixpunkt zurück
     return next_x_value
 
 
-def jacobi_mit_anzahl_iterationen(startvektor, anzahl_iterationen):
+def jacobi_mit_anzahl_iterationen(startvektor, anzahl_iterationen, debug=False):
     previous_value = startvektor
     next_x_value = -100
     x_values = [previous_value]
+
+    if debug:
+        print_jacobi_zwischenschritte()
 
     for i in range(anzahl_iterationen):
         # Nur für die erste Iteration nicht machen
         if i > 0:
             previous_value = next_x_value
-        next_x_value = jacobi(previous_value)
+        if debug:
+            print(f"Jacobi Iteration: {i + 1}")
+            print(f"x^{i} = \n{previous_value.reshape(-1, 1)}")
+        next_x_value = jacobi(previous_value, debug)
         x_values.append(next_x_value)
-        print(f"x^{i} = {previous_value} -> {next_x_value}")
+        print(f"= \n{next_x_value.reshape(-1, 1)}")
     return x_values
 
+
 # Variablen definieren
-startvektor = np.array([1, -1, 3])
-A = np.array([[8, 5, 2], [5, 9, 1], [4, 2, 7]])
-b = np.array([19, 5, 34])
-[L, R, D] = A_Matrix_in_D_L_R_unterteilen.unterteilen(A)
+# startvektor = np.array([4, 3, -0.5])
+# A = np.array([[2, -1, 0], [-1, 4, 1], [0, 1, 2]])
+# b = np.array([4, 5, -2])
+A = np.array([[3, 2], [1, 3]])
+b = np.array([1, 0])
+startvektor = np.array([1, 1])
+[L, R, D] = A_Matrix_in_D_L_R_unterteilen.unterteilen(A, True)
 # Toleranz definieren
 toleranz = 10**-4
 
 
 # Fixpunkt mit Toleranz berechnen
 # fixpunkt = jacobi_mit_toleranz(startvektor)
-# jacobi_mit_anzahl_iterationen(startvektor, 5)
+jacobi_mit_anzahl_iterationen(startvektor, 1, True)
 # print("Fixpunkt: ", fixpunkt)
