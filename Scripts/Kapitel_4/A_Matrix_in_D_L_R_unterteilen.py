cdddaea0f5c7bc4b1c8084627cf992febd6e43b9
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2024-01-03 22:13:13 +0100

    refactor: adjust debug output

--- Scripts/Kapitel_4/A_Matrix_in_D_L_R_unterteilen.py
+++ Scripts/Kapitel_4/A_Matrix_in_D_L_R_unterteilen.py
@@ -5,8 +5,10 @@ def unterteilen(A, debug=False):
     D = np.diag(np.diag(A))
     R = np.triu(A) - D
     L = np.tril(A) - D
+
     if debug:
-        print(f"L: \n {L}")
-        print(f"D: \n {D}")
-        print(f"R: \n {R}")
+        print(f"L = \n {L}")
+        print(f"D = \n {D}")
+        print(f"R = \n {R}")
+
     return L, R, D
--- Scripts/Kapitel_4/Jacobi.py
+++ Scripts/Kapitel_4/Jacobi.py
@@ -1,96 +1,119 @@
-import A_Matrix_in_D_L_R_unterteilen
 import numpy as np
 import numpy.linalg as lin
-from sympy import false, true
 
 
-def jacobi(prev_x, debug=False):
+def a_in_dlr_zerlegen(A, debug=False):
+    D = np.diag(np.diag(A))
+    L = np.tril(A) - D
+    R = np.triu(A) - D
+
     if debug:
-        print(
-            f"-> \n{-lin.inv(D).dot(L + R)}\n * \n{prev_x.reshape(-1, 1)}\n + \n{(lin.inv(D).dot(b)).reshape(-1, 1)}\n"
-        )
-    return -lin.inv(D).dot(L + R).dot(prev_x) + lin.inv(D).dot(b)
+        print("-- A in D^-1, L, R zerlegen")
+        print(f"D^-1 = \n {lin.inv(D)}")
+        print(f"L + R = \n {L + R}")
+        print()
+
+    return D, L, R
 
 
-def print_jacobi_zwischenschritte():
-    print("---------------------------------------")
-    print("x^k+1 = -inv(D) * (L + R) * x^0 + inv(D) * b \n")
-    print(
-        f"= -inv(\n{D}\n) * \n{L + R}\n * x_k + inv(\n{D}\n) * \n{b.reshape(-1, 1)}\n"
-    )
-    print("Vereinfacht: ")
-    print(
-        f"= \n{-lin.inv(D)}\n * \n{L + R}\n * x_k + \n{lin.inv(D)}\n * \n{b.reshape(-1, 1)}\n"
-    )
-    print("Vereinfacht: ")
-    print(
-        f"= \n{-lin.inv(D).dot(L + R)}\n * x_k + \n{(lin.inv(D).dot(b)).reshape(-1, 1)}\n"
-    )
+def dlr_zu_bc(D, L, R, b, debug=False):
+    B = -lin.inv(D).dot(L + R)
+    C = lin.inv(D).dot(b.reshape(-1, 1))
+
+    if debug:
+        print("-- Von D, L, R zu B, C")
+        print(f"B = -D^-1 * (L + R) = \n {B}")
+        print(f"C = D^-1 * b = \n {C}")
+        print()
+
+    return B, C
 
 
-def vergleiche_toleranz(prev_value, next_value):
+def jacobi_iteration(B, C, prev_x, debug=False):
+    if debug:
+        print(f" {B} \n * \n {prev_x.reshape(-1, 1)} \n + \n {C}")
+
+    return B.dot(prev_x.reshape(-1, 1)) + C
+
+
+def vergleiche_toleranz(prev_value, next_value, toleranz):
     for i in range(len(prev_value)):
         if np.abs(prev_value[i] - next_value[i]) > toleranz:
-            return false
-    return true
+            return False
+
+    return True
 
 
-def jacobi_mit_toleranz(startvektor, debug=False):
-    previous_value = startvektor
+def jacobi_mit_toleranz(A, b, x_0, toleranz, debug=False):
+    previous_value = x_0
     next_x_value = np.full(len(previous_value), -100)
     iteration_count = 0
 
-    if debug:
-        print_jacobi_zwischenschritte()
+    [D, L, R] = a_in_dlr_zerlegen(A, debug)
+    [B, C] = dlr_zu_bc(D, L, R, b, debug)
 
-    while not vergleiche_toleranz(previous_value, next_x_value):
+    while not vergleiche_toleranz(previous_value, next_x_value, toleranz):
         # Nur für die erste Iteration nicht machen
         if iteration_count > 0:
             previous_value = next_x_value
+
         if debug:
-            print(f"Jacobi Iteration: {iteration_count + 1}")
-            print(f"x^{iteration_count} = \n{previous_value.reshape(-1, 1)}")
-        next_x_value = jacobi(previous_value, debug)
-        print(f"= \n{next_x_value.reshape(-1, 1)}")
+            print(f"--------------------- Iteration: {iteration_count + 1}")
+            print(f"x^{iteration_count + 1} = B * x^{iteration_count} + C =")
+
+        next_x_value = jacobi_iteration(B, C, previous_value, debug)
+        print(f" = \n {next_x_value}")
+        print()
+
         iteration_count = iteration_count + 1
-    # Gebe Fixpunkt zurück
+
     return next_x_value
 
 
-def jacobi_mit_anzahl_iterationen(startvektor, anzahl_iterationen, debug=False):
-    previous_value = startvektor
+def jacobi_mit_anzahl_iterationen(A, b, x_0, iterationen, debug=False):
+    previous_value = x_0
     next_x_value = -100
     x_values = [previous_value]
 
-    if debug:
-        print_jacobi_zwischenschritte()
+    [D, L, R] = a_in_dlr_zerlegen(A, debug)
+    [B, C] = dlr_zu_bc(D, L, R, b, debug)
 
-    for i in range(anzahl_iterationen):
+    for i in range(iterationen):
         # Nur für die erste Iteration nicht machen
         if i > 0:
             previous_value = next_x_value
+
         if debug:
-            print(f"Jacobi Iteration: {i + 1}")
-            print(f"x^{i} = \n{previous_value.reshape(-1, 1)}")
-        next_x_value = jacobi(previous_value, debug)
+            print(f"--------------------- Iteration: {i + 1}")
+            print(f"x^{i + 1} = B * x^{i} + C =")
+
+        next_x_value = jacobi_iteration(B, C, previous_value, debug)
         x_values.append(next_x_value)
-        print(f"= \n{next_x_value.reshape(-1, 1)}")
+        print(f" = \n {next_x_value}")
+        print()
+
     return x_values
 
 
-# Variablen definieren
-# startvektor = np.array([4, 3, -0.5])
-# A = np.array([[2, -1, 0], [-1, 4, 1], [0, 1, 2]])
-# b = np.array([4, 5, -2])
-A = np.array([[3, 2], [1, 3]])
-b = np.array([1, 0])
-startvektor = np.array([1, 1])
-[L, R, D] = A_Matrix_in_D_L_R_unterteilen.unterteilen(A, True)
+########################################################################################
+
+# Matrix A definieren
+A = np.array([[4, -1, 1], [-2, 5, 1], [1, -2, 5]])
+
+# Vektor b definieren
+b = np.array([5, 11, 12])
+
+# Startvektor x_0 definieren
+x_0 = np.array([0, 0, 0])
+
+# Fixpunkt mit Toleranz berechnen
 # Toleranz definieren
 toleranz = 10**-4
 
+jacobi_mit_toleranz(A, b, x_0, toleranz, True)
 
-# Fixpunkt mit Toleranz berechnen
-# fixpunkt = jacobi_mit_toleranz(startvektor)
-jacobi_mit_anzahl_iterationen(startvektor, 1, True)
-# print("Fixpunkt: ", fixpunkt)
+# Fixpunkt mit angegebener Anzahl Iterationen berechnen
+# Anzahl der Iterationen
+# iterationen = 2
+
+# jacobi_mit_anzahl_iterationen(A, b, x_0, iterationen, True)
