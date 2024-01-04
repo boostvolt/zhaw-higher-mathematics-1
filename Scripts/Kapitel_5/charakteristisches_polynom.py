dc6dce2badd85b2f13c65be35ba033e15aa7a329
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2024-01-04 09:31:08 +0100

    refactor: rename I to E

--- Scripts/Kapitel_5/charakteristisches_polynom.py
+++ Scripts/Kapitel_5/charakteristisches_polynom.py
@@ -5,14 +5,14 @@ import sympy as sp
 def charakteristisches_polynom(A, debug=False):
     A = sp.matrices.Matrix(A)
     lambda_symbol = sp.symbols("λ")
-    I = sp.matrices.eye(A.shape[0])
+    E = sp.matrices.eye(A.shape[0])
     polynom = A.charpoly(lambda_symbol).as_expr()
     factorized_polynom = sp.factor(polynom)
 
     if debug:
         print("-- Charakteristisches Polynom")
         print(
-            f"A - λI_{A.shape[0]} = \n {np.array(A)} \n - λ * \n {np.array(I)} \n = \n {np.array(A - lambda_symbol * I)}"
+            f"A - λI_{A.shape[0]} = \n {np.array(A)} \n - λ * \n {np.array(E)} \n = \n {np.array(A - lambda_symbol * E)}"
         )
         print(f"det(A - λI_{A.shape[0]}) = p(λ) = {polynom} = {factorized_polynom}")
 
