249aa0431077417564c8820312302f50b696985e
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-01-04 15:00:55 +0100

    improv: Add Jacobi and Gauss-Seidel Spektralradius

--- Scripts/Kapitel_5/spektralradius.py
+++ Scripts/Kapitel_5/spektralradius.py
@@ -1,4 +1,20 @@
 import numpy as np
+import numpy.linalg as lin
+
+
+def a_in_ldr_zerlegen(A):
+    D = np.diag(np.diag(A))
+    L = np.tril(A) - D
+    R = np.triu(A) - D
+    return L, D, R
+
+
+def ldr_zu_b_jacobi(L, D, R):
+    return -lin.inv(D).dot(L + R)
+
+
+def ldr_zu_b_gauss_seidel(L, D, R):
+    return -lin.inv(L + D).dot(R)
 
 
 def spektral_radius(matrix, debug=False):
@@ -16,6 +32,30 @@ def spektral_radius(matrix, debug=False):
 ########################################################################################
 
 # Matrix definieren
-matrix = np.array([[1, 0, 0], [2, 3, 0], [0, 1, 2]])
+A = np.array([[1, 0, 0], [2, 3, 0], [0, 1, 2]])
 
-print(f"Spektralradius: {spektral_radius(matrix, True)}")
+print(f"Spektralradius: {spektral_radius(A, debug=True)}")
+
+
+# Wenn Konvergenz von Jacobi oder Gauss-Seidel untersucht werden soll anhand dem Spektralradius
+# Matrix definieren
+A = np.array([[3, 2, 1], [2, 3, 2], [1, 2, 3]])
+L, D, R = a_in_ldr_zerlegen(A)
+
+# Jacobi
+B = ldr_zu_b_jacobi(
+    L,
+    D,
+    R,
+)
+
+print(f"Spektralradius für Jacobi: {spektral_radius(B, debug=False)}")
+
+# Gauss-Seidel
+B = ldr_zu_b_gauss_seidel(
+    L,
+    D,
+    R,
+)
+
+print(f"Spektralradius für Gauss-Seidel: {spektral_radius(B, debug=False)}")
