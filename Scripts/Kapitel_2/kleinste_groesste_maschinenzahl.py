2dbb2df9cfb2b715ecd8d68ce1eb72f3f55911d3
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-01-09 00:03:15 +0100

    add basis unabhängigkeit des exponenten und andere verbesserungen

--- Scripts/Kapitel_2/kleinste_groesste_maschinenzahl.py
+++ Scripts/Kapitel_2/kleinste_groesste_maschinenzahl.py
@@ -1,55 +1,58 @@
-def l_min(number_of_digits, debug=False):
-    smallest_binary_number = "-" + "1" * (number_of_digits)
+def get_l_min(basis_exponent, exponent_stellen, debug=False):
+    groesste_zahl_im_system = basis_exponent - 1
+    smallest_binary_number = "-" + f"{groesste_zahl_im_system}" * (exponent_stellen)
     if debug:
         print(
-            f"Kleinste Zahl im Dualsystem mit {number_of_digits} Stellen: 0.1 * 2^{smallest_binary_number}"
+            f"Kleinste Zahl im {basis_exponent}-System mit {exponent_stellen} Stellen: 0.1 * {basis_exponent}^{smallest_binary_number}"
         )
-    decimal_value = int(smallest_binary_number, 2)
+    decimal_value = int(smallest_binary_number, basis_exponent)
     return decimal_value
 
 
-def l_max(number_of_digits, debug=False):
-    smallest_binary_number = "1" * (number_of_digits)
+def get_l_max(basis_exponent, exponent_stellen, debug=False):
+    groesste_zahl_im_system = basis_exponent - 1
+    smallest_binary_number = f"{groesste_zahl_im_system}" * (exponent_stellen)
     if debug:
         print(
-            f"Groesste Zahl im Dualsystem mit {number_of_digits} Stellen: 2^{smallest_binary_number}"
+            f"Groesste Zahl im {basis_exponent}-System mit {exponent_stellen} Stellen: {basis_exponent}^{smallest_binary_number}"
         )
-    decimal_value = int(smallest_binary_number, 2)
+    decimal_value = int(smallest_binary_number, basis_exponent)
     return decimal_value
 
 
-def kleinste_positive_maschinenzahl(basis, exponent_stellen, debug=False):
+def kleinste_positive_maschinenzahl(
+    basis, basis_exponent, exponent_stellen, debug=False
+):
+    l_min = get_l_min(basis_exponent, exponent_stellen, debug)
     if debug:
-        print(
-            f"B^(l_min - 1): {basis}^({l_min(exponent_stellen, debug)} - {1}) = {basis}^({l_min(exponent_stellen) - 1})"
-        )
-        print(
-            f"Kleinste positive Maschinenzahl: {basis ** (l_min(exponent_stellen) - 1)}"
-        )
-    return basis ** (l_min(exponent_stellen) - 1)
+        print(f"B^(l_min - 1): {basis}^({l_min} - {1}) = {basis}^({l_min - 1})")
+        print(f"Kleinste positive Maschinenzahl: {basis ** (l_min - 1)}")
+    return basis**l_min - 1
 
 
 def groesste_positive_maschinenzahl(
-    basis, mantisse_stellen, exponent_stellen, debug=False
+    basis, basis_exponent, mantisse_stellen, exponent_stellen, debug=False
 ):
+    l_max = get_l_max(basis_exponent, exponent_stellen, debug)
     if debug:
         print(
-            f"B^l_max - B^(l_max - n): {basis}^{l_max(exponent_stellen, debug)} - {basis}^{l_max(exponent_stellen)} - {mantisse_stellen}) = {basis}^{l_max(exponent_stellen)} - {basis}^{l_max(exponent_stellen) - mantisse_stellen})"
+            f"B^l_max - B^(l_max - n): {basis}^{l_max} - {basis}^({l_max} - {mantisse_stellen}) = {basis}^{l_max} - {basis}^{l_max - mantisse_stellen}"
         )
         print(
-            f"Groesste positive Maschinenzahl: {basis ** l_max(exponent_stellen) - (basis ** (l_max(exponent_stellen) - mantisse_stellen))}"
+            f"Groesste positive Maschinenzahl: {(basis ** l_max) - (basis ** (l_max - mantisse_stellen))}"
         )
-    return basis ** l_max(exponent_stellen) - (
-        basis ** (l_max(exponent_stellen) - mantisse_stellen)
-    )
+    return (basis**l_max) - (basis ** (l_max - mantisse_stellen))
 
 
-# Achtung: Geht momentan nur im Dualsystem, weil wir keine andere Beispiele / Aufgaben
-# hatten die mit einer anderen Basis oder anderen Exponenten-Basis gerechnet wurden
+# Achtung: Diese Funktion funktioniert für Dualsysteme für Basis und Exponent.
+# Für alle anderen Systeme ist es nicht garantiert, dass die Lösung stimmt, weil wir keine Aufgaben hatten mit der wir es überprüfen konnten.
 
-mantisse_stellen = 2
-exponent_stellen = 1
+mantisse_stellen = 20
+exponent_stellen = 4
 basis = 2
+basis_exponent = 2
 
-kleinste_positive_maschinenzahl(basis, exponent_stellen, True)
-groesste_positive_maschinenzahl(basis, mantisse_stellen, exponent_stellen, True)
+kleinste_positive_maschinenzahl(basis, basis_exponent, exponent_stellen, True)
+groesste_positive_maschinenzahl(
+    basis, basis_exponent, mantisse_stellen, exponent_stellen, True
+)
