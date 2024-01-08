d5ecde700b8fff0f09b02f7556b42b8d78e0a299
sunnyfisch <65352564+sunnyfisch@users.noreply.github.com>
2024-01-08 09:53:10 +0100

    Add bugfix für strikte Diagonaldominanz

--- Scripts/Kapitel_4/diagonaldominanz.py
+++ Scripts/Kapitel_4/diagonaldominanz.py
@@ -14,11 +14,13 @@ def check_zeilendominanz(rows, debug=False):
     for i in range(rows):
         diagonal = abs(matrix[i, i])
         non_diagonal_sum = np.sum(abs(matrix[i, :])) - diagonal
-        if diagonal < non_diagonal_sum:
+        if diagonal <= non_diagonal_sum:
             return False
 
     if debug:
-        print("Zeilendominant")
+        print()
+        print("Zeilensummenkriterium")
+        print("für alle i = 1, ...., n |a_ii| > ∑^n _j=1, j != i |a_i,j|")
         print()
 
     return True
@@ -28,11 +30,13 @@ def check_spaltendominanz(cols, debug=False):
     for i in range(cols):
         diagonal = abs(matrix[i, i])
         non_diagonal_sum = np.sum(abs(matrix[:, i])) - diagonal
-        if diagonal < non_diagonal_sum:
+        if diagonal <= non_diagonal_sum:
             return False
 
     if debug:
-        print("Spaltendominant")
+        print()
+        print("Spaltensummenkriterium")
+        print("für alle j = 1, ...., n |a_jj| > ∑^n _i=1, i != j |a_i,j|")
         print()
 
     return True
@@ -43,4 +47,5 @@ def check_spaltendominanz(cols, debug=False):
 # Matrix definieren
 matrix = np.array([[8, 5, 2], [5, 9, 1], [4, 2, 7]])
 
-print(f"Diagonaldominanz: {is_diagonaldominant(matrix, True)}")
+print(f"Strikte Diagonaldominanz: {is_diagonaldominant(matrix, True)}")
+print("Falls A diagonaldominant ist, konvergiert das Gesamtschrittverfahren (Jacobi) und auch das Einzelschrittverfahren (Gauss-Seidel) für Ax = b.")
