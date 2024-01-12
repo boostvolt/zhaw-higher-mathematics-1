3184a6b0dc3d12ec6d7323b03ef62a259dfeafcf
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2024-01-12 16:15:17 +0100

    run ruff

--- Scripts/Basics/inverse.py
+++ Scripts/Basics/inverse.py
@@ -1,6 +1,6 @@
-import numpy as np 
+import numpy as np
 
 # Matrix definieren
 A = np.array([[4, 1, 0], [3, 2, 1], [5, 2, -1]])
 
-print('Die Inverse von ${} lautet: ${}'.format(A,np.linalg.inv(A)))
+print("Die Inverse von ${} lautet: ${}".format(A, np.linalg.inv(A)))
